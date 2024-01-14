bd01c6be77dccd6d4396f20d3e3bb1e017925605
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-01-14 20:52:16 +0530

    form modifier

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,7 @@
   <UserToParentTop />
   <refComponent />
   <FormComponent />
+  <formModifierComponent />
   
 </template>
 
@@ -37,6 +38,7 @@ import ClassBinding from './components/ClassBinding.vue';
 import UserToParentTop from './components/UserToParentTop.vue';
 import refComponent from './components/refComponent.vue';
 import FormComponent from './components/formComponent.vue';
+import formModifierComponent from './components/formModifierComponent.vue';
 
 
 
@@ -59,7 +61,8 @@ export default {
     ClassBinding,
     UserToParentTop,
     refComponent,
-    FormComponent
+    FormComponent,
+    formModifierComponent
   }
 }
 </script>
---
+++ src/components/formModifierComponent.vue
@@ -0,0 +1,29 @@
+<template>
+<h1>This concept is form modifier</h1>
+<label>while remove focus from text field</label><br>
+<input type="text" v-model.lazy="data" /><br><br>
+<label>remove the space from the text field</label><br>
+<input type="number" v-model.trim="removeSpace" />
+<h2>The input is-->{{ data }}</h2>
+<h2>Remove Space from text ::{{ typeof removeSpace }}</h2>
+
+</template>
+
+<script>
+export default {
+    name:'formModifierComponent',
+    data(){
+        return {
+            data:'',
+            removeSpace:''
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1{
+    background-color: rgb(60, 9, 128);
+    color: white;
+}
+</style>
