4960f4c004e7bb2e06cd491d9bb86461b44c04f0
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-01-19 21:34:51 +0530

    multiple slot has been used by Name:

--- src/components/SlotChildComp.vue
+++ src/components/SlotChildComp.vue
@@ -1,19 +1,36 @@
 <template>
-<h2>{{ data }}</h2>    
-<slot></slot>
+    <h2>{{ data }}</h2>
+    <slot></slot>
+
+    <!-- multiple slot by name example -->
+    <div class="slot-container">
+        <slot name="header"></slot>
+        <slot name="main"></slot>
+        <slot name="footer"></slot>
+
+    </div>
 </template>
 
 <script>
-export default{
-    name:'SlotChildComp',
-   props:{
-    data:String
-   } 
+export default {
+    name: 'SlotChildComp',
+    props: {
+        data: String
+    }
 }
 </script>
 
-<style>
-h1{
+<style scoped>
+h1 {
     background-color: rgb(78, 78, 4);
+}
+.slot-container{
+    border: 1px solid blue;
+    width: 15%;
+    height: 33vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 }
 </style>
--- src/components/SlotParentComp.vue
+++ src/components/SlotParentComp.vue
@@ -7,6 +7,44 @@
 <SlotChildComp>
     <img src="https://hips.hearstapps.com/hmg-prod/images/beautiful-smooth-haired-red-cat-lies-on-the-sofa-royalty-free-image-1678488026.jpg?crop=0.88847xw:1xh;center,top&resize=1200:*" alt="">
 </SlotChildComp>
+
+<h1>Multiple Slots By Name</h1>
+<!-- multiple slot by name -->
+<SlotChildComp>
+    <template v-slot:header>
+        <h2>First Parameter</h2>
+    </template>
+    <template v-slot:main>
+        <p>This is the main component</p>
+    </template>
+    <template v-slot:footer>
+        <button>Apply</button>
+    </template>
+</SlotChildComp>
+
+<SlotChildComp>
+    <template v-slot:header>
+        <h2>Second Parameter</h2>
+    </template>
+    <template v-slot:main>
+        <p>This is the main second component</p>
+    </template>
+    <template v-slot:footer>
+        <button>Btn</button>
+    </template>
+</SlotChildComp>
+
+<SlotChildComp>
+    <template v-slot:header>
+        <h2>Third Parameter</h2>
+    </template>
+    <template v-slot:main>
+        <p>This is the main third component</p>
+    </template>
+    <template v-slot:footer>
+        <button>Buttons</button>
+    </template>
+</SlotChildComp>
 </template>
 
 <script>
